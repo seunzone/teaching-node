b1e8fc7c1e956b98b987152abb3f82b14e8f23c2
seunzone <darealseun@gmail.com>
2017-08-09 12:53:53 +0100

    working with node module

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 var http = require('http');
+var cat = require('cat-me');
+var knock = require('knock-knock-jokes');
+
+console.log(cat());
+console.log(knock());
 
 //create a server object:
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hello World!</h1>');
-}).listen(3000);
+//http.createServer(function (req, res) {
+//    res.writeHead(200, {'Content-Type': 'text/html'});
+//    res.end('<h1>Hello World!</h1>');
+//}).listen(3000);
+
